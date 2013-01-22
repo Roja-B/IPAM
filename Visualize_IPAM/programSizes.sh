a232af909985647f85c9a4d714aa74c620fa6a1b
Roja-ucla <roja@ucla.edu>
2013-01-21 19:41:03 -0800

    corrected a bug that gave wrong program sizes

--- Visualize_IPAM/programSizes.sh
+++ Visualize_IPAM/programSizes.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-prognames=$(more myRawData/INCLUDElist)
+prognames=$(more myIntermediateData/EdgeList_resolved)
 echo $prognames
 for progname in $prognames; do
 	grep $progname myIntermediateFiles/EdgeList_resolved | wc -l
